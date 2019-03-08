19f0a1403683ced60c6a30db2171238c2b9d95a1
kocurass@gmail.com <k.kapalka@hotmail.com>
2019-03-08 16:05:35 +0100

    navbar

---
+++ navbar.js
@@ -0,0 +1,6 @@
+let nav = '<nav><a href="index.html">Home</a>'+
+    '<a href="#">Blog</a>'+
+    '<a href="#">Quiz</a>'+
+    '<a href="contact.html">Kontakt</a></nav>';
+let navBar = document.querySelector(".navbar");
+navBar.innerHTML = nav;
