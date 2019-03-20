ddbf52e366452274802d181de1bc983d3ddb7901
Velkan <achwatm@gmail.com>
2019-03-20 18:09:02 +0100

    scroll

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
-let nav = '<nav><a href="../index.html">Home</a>'+
+let nav = '<nav><a href="index.html">Home</a>'+
     '<a href="#">Blog</a>'+
-    '<a href="#">Quiz</a>'+
-    '<a href="../contact.html">Kontakt</a></nav>';
+    '<a href="quiz.html">Quiz</a>'+
+    '<a href="contact.html">Kontakt</a></nav>';
 let navBar = document.querySelector(".navbar");
 navBar.innerHTML = nav;
